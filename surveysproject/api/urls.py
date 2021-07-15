627f6abe785a14b46c426d813cf335ae76ae8963
Pavel Zakharov <pzakharov83@gmail.com>
2021-07-15 07:27:27 +0300

    added routes

--- surveysproject/api/urls.py
+++ surveysproject/api/urls.py
@@ -2,7 +2,8 @@ from django.urls import include, path
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
-from .views import SurveyViewSet, QuestionViewSet, AnswerViewSet
+from .views import (SurveyViewSet, QuestionViewSet, AnswerViewSet,
+                    SurveyInstanceViewSet, CustomerViewSet)
 
 router_v1 = DefaultRouter()
 router_v1.register(r'survey', SurveyViewSet, basename='survey')
@@ -14,6 +15,8 @@ router_v1.register(
     r'questions/(?P<survey_id>[\d]+)/answers',
     AnswerViewSet, basename='answers'
 )
+router_v1.register(r'instance', SurveyInstanceViewSet, basename='instance')
+router_v1.register(r'customer', CustomerViewSet, basename='customer')
 
 urlpatterns = [
     path('v1/api-token-auth/', views.obtain_auth_token),
